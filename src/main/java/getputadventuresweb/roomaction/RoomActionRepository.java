993b8a0df53e5d5db724f888d8ed33ee53e8117b
UnquiteBones <me@martha.net>
2020-11-25 12:30:30 -0500

    Changes to Action SQL
    The actions returned are now filtered by the inventory list based on the requiredID and forbiddenID so updated the SQL... (also made it easier to read)

--- src/main/java/getputadventuresweb/roomaction/RoomActionRepository.java
+++ src/main/java/getputadventuresweb/roomaction/RoomActionRepository.java
@@ -12,12 +12,31 @@ import java.util.List;
 @Repository
 public interface RoomActionRepository extends JpaRepository<RoomActionEntity, Integer>, JpaSpecificationExecutor<RoomActionEntity> {
 
-    @Query("SELECT t FROM RoomActionEntity t WHERE t.roomID = :roomID")
+    @Query("SELECT t " +
+            "FROM RoomActionEntity t " +
+            "WHERE t.roomID = :roomID")
     public List<RoomActionEntity> findByRoomId(@Param("roomID") Integer roomID);
 
-    @Query("SELECT t2 FROM RoomActionEntity t1 JOIN ActionEntity t2 ON t1.actionID = t2.id WHERE t1.roomID = :roomID")
+    @Query("SELECT t2 " +
+            "FROM RoomActionEntity t1 " +
+            "JOIN ActionEntity t2 ON t1.actionID = t2.id " +
+            "WHERE t1.roomID = :roomID ")
     public List<ActionEntity> findActionsByRoomId(@Param("roomID") Integer roomID);
 
-    @Query("UPDATE RoomActionEntity t SET t.roomID = :resetRoomId WHERE t.actionID = :actionID")
+    @Query("SELECT t2 " +
+            "FROM RoomActionEntity t1 " +
+            "JOIN ActionEntity t2 ON t1.actionID = t2.id " +
+            "WHERE t1.roomID = :roomID " +
+            "AND ((t2.forbidsActionId IS NULL) " +
+                "OR ((t2.forbidsActionId IS NOT NULL) " +               // forbidden action
+                    "AND (t2.forbidsActionId NOT IN (:invList))))" +     // or not in InvList
+            "AND ((t2.requiresActionId IS NULL) "+
+                "OR (t2.requiresActionId IS NOT NULL "+             // required action
+                    "AND (t2.requiresActionId IN (:invList))))")    // required action is in InvList
+    public List<ActionEntity> findActionsByRoomId(@Param("roomID") Integer roomID, @Param("invList") List<Integer> invList);
+
+    @Query("UPDATE RoomActionEntity t " +
+            "SET t.roomID = :resetRoomId " +
+            "WHERE t.actionID = :actionID")
     public void resetActionRoom(@Param("actionID") Integer roomID, @Param("resetRoomId") Integer resetRoomId);
 }

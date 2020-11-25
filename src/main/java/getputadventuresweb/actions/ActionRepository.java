c098b8e4c8a0e76e84fa146d13365835d46ea7ba
UnquiteBones <me@martha.net>
2020-11-25 12:27:07 -0500

    Pulled out unused imports

--- src/main/java/getputadventuresweb/actions/ActionRepository.java
+++ src/main/java/getputadventuresweb/actions/ActionRepository.java
@@ -1,12 +1,7 @@
 package getputadventuresweb.actions;
 
-import getputadventuresweb.roomaction.RoomActionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ActionRepository extends JpaRepository<ActionEntity, Integer> {
